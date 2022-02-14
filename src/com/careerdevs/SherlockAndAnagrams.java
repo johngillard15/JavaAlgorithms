b07d494f717a1ac6a48a24a4df1e71f95d2507e6
JG <johngillard15@gmail.com>
2022-02-14 13:05:37 -0500

    updated comments and variables

--- src/com/careerdevs/SherlockAndAnagrams.java
+++ src/com/careerdevs/SherlockAndAnagrams.java
@@ -9,38 +9,37 @@ public class SherlockAndAnagrams {
         int pairs = 0;
 
         for(int i = 0; i < s.length(); i++){
-            HashMap<Character, Integer> wordCount = new HashMap<>();
+            HashMap<Character, Integer> totalCount = new HashMap<>();
 
             // check all substrings up to current letter for anagrams
             for(int end = i; end >= 0; end--){
-                if(wordCount.containsKey(s.charAt(end)))
-                    wordCount.put(s.charAt(end), wordCount.get(s.charAt(end)) + 1);
+                if(totalCount.containsKey(s.charAt(end)))
+                    totalCount.put(s.charAt(end), totalCount.get(s.charAt(end)) + 1);
                 else
-                    wordCount.put(s.charAt(end), 1);
+                    totalCount.put(s.charAt(end), 1);
 
                 // count the letters in substring(start, end)
                 for(int start = 0; start < end; start++){
                     HashMap<Character, Integer> substringCount = new HashMap<>();
-                    int x = start;
+                    int subStart = start;
 
                     for(int count = 0; count <= i - end; count++){
-                        if(substringCount.containsKey(s.charAt(x)))
-                            substringCount.put(s.charAt(x), substringCount.get(s.charAt(x)) + 1);
+                        if(substringCount.containsKey(s.charAt(subStart)))
+                            substringCount.put(s.charAt(subStart), substringCount.get(s.charAt(subStart)) + 1);
                         else
-                            substringCount.put(s.charAt(x), 1);
-                        x++;
+                            substringCount.put(s.charAt(subStart), 1);
+                        subStart++;
                     }
 
                     // if the letter count of the current substring does not match the total word,
                     // the string is not an anagram
                     boolean isAnagram = true;
-                    for(Map.Entry<Character, Integer> letter : substringCount.entrySet()){
-                        if(letter.getValue() != wordCount.get(letter.getKey())){
+                    for(Map.Entry<Character, Integer> letter : totalCount.entrySet()){
+                        if(letter.getValue() != substringCount.get(letter.getKey())){
                             isAnagram = false;
                             break;
                         }
                     }
-
                     if(isAnagram)
                         pairs++;
                 }
