eeaf5d6507893e0cfd06071e9cc0c3df6c1b1256
JG <johngillard15@gmail.com>
2022-02-15 23:09:45 -0500

    Create RemoveKFromList.java

---
+++ src/com/careerdevs/RemoveKFromList.java
@@ -0,0 +1,23 @@
+package com.careerdevs;
+
+class ListNode<T>{
+   ListNode(T x) {
+     value = x;
+   }
+
+   T value;
+   ListNode<T> next;
+ }
+
+public class RemoveKFromList {
+
+    public static ListNode<Integer> solution(ListNode<Integer> l, int k){
+        ListNode<Integer> head;
+
+        return l;
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
