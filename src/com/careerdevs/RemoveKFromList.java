bf2e84d8cd772da3e279ccb072a8032b400eff93
JG <johngillard15@gmail.com>
2022-02-22 19:59:00 -0500

    changed solution

--- src/com/careerdevs/RemoveKFromList.java
+++ src/com/careerdevs/RemoveKFromList.java
@@ -3,13 +3,18 @@ package com.careerdevs;
 public class RemoveKFromList {
 
     public static ListNode<Integer> solution(ListNode<Integer> head, int k){
-        if(head == null)
-            return null;
+        while(head != null && head.value == k)
+            head = head.next;
 
-        head.next = solution(head.next, k);
+        ListNode<Integer> node = head;
 
-        if(head.value == k){
-            return head.next;
+        if(node != null){
+            while(node.next != null){
+                if(node.next.value == k)
+                    node.next = node.next.next;
+                else
+                    node = node.next;
+            }
         }
 
         return head;
