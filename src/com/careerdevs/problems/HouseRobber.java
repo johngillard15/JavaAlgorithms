e20e96d5361d65d261d3f22afb8bf022ae6c543d
JG <johngillard15@gmail.com>
2022-03-17 21:33:34 -0400

    Update HouseRobber.java

--- src/com/careerdevs/problems/HouseRobber.java
+++ src/com/careerdevs/problems/HouseRobber.java
@@ -6,15 +6,21 @@ import java.util.List;
 public class HouseRobber {
 
     public static int solution(int[] nums) {
-        List<Integer> houses = new ArrayList<>();
+        if(nums.length == 0)
+            return 0;
+        if(nums.length == 1)
+            return nums[0];
+        // if(nums.length == 2)
+        //     return Math.max(nums[0], nums[1]);
 
-        for(int i = 0; i < nums.length; i++){
-            if(houses.indexOf(i) && nums[i] > nums[bestHouse])
-                bestHouse = i;
+        int lastHouse = nums[0];
+        int lastHouse2 = 0;
+        for(int i = 1; i < nums.length; i++){
+            int thisHouse = Math.max(lastHouse2 + nums[i], lastHouse);
+            lastHouse2 = lastHouse;
+            lastHouse = thisHouse;
         }
 
-        int money = 0;
-
-        return bestHouse;
+        return lastHouse;
     }
 }
