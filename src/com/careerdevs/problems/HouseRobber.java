78b62c82c91926ed9389b107d1db86fde0d80cb5
JG <johngillard15@gmail.com>
2022-03-25 15:40:31 -0400

    Update HouseRobber.java

--- src/com/careerdevs/problems/HouseRobber.java
+++ src/com/careerdevs/problems/HouseRobber.java
@@ -1,26 +1,32 @@
 package com.careerdevs.problems;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class HouseRobber {
 
     public static int solution(int[] nums) {
-        if(nums.length == 0)
-            return 0;
-        if(nums.length == 1)
-            return nums[0];
-        // if(nums.length == 2)
-        //     return Math.max(nums[0], nums[1]);
-
-        int lastHouse = nums[0];
-        int lastHouse2 = 0;
-        for(int i = 1; i < nums.length; i++){
-            int thisHouse = Math.max(lastHouse2 + nums[i], lastHouse);
-            lastHouse2 = lastHouse;
-            lastHouse = thisHouse;
+        switch(nums.length){
+            case 0:
+                return 0;
+            case 1:
+                return nums[0];
+            case 2:
+                return Math.max(nums[0], nums[1]);
+            case 3:
+                return Math.max(nums[0] + nums[2], nums[1]);
+            default:
+                break;
         }
 
-        return lastHouse;
+        int maxHaul = 0;
+        int lastHaul = nums[0];
+        int lastHaul2 = 0;
+        for(int i = 1; i < nums.length; i++){
+            maxHaul = Math.max(lastHaul2 + nums[i], lastHaul);
+            lastHaul2 = lastHaul;
+            lastHaul = maxHaul;
+        }
+
+        return maxHaul;
     }
+
+
 }
